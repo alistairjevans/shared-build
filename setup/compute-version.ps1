b84588ecb9f3e794dca5649bc4b32e9aec46ee0b
alistairjevans <alistairjevans@gmail.com>
2021-10-30 16:52:15 +0100

    Printing dotnet info

--- setup/compute-version.ps1
+++ setup/compute-version.ps1
@@ -3,4 +3,4 @@ $branch = @{ $true = $env:GITHUB_REF; $false = $(git symbolic-ref --short -q HEA
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:GITHUB_RUN_NUMBER, 10); $false = "local" }[$NULL -ne $env:GITHUB_RUN_NUMBER];
 $versionSuffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)).Replace('/', '-'))-$revision" }[$branch -eq "master" -and $revision -ne "local"]
 
-"::set-output name=versionSuffix::$versionSuffix"
+echo "::set-output name=versionSuffix::$versionSuffix"
