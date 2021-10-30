e5389894d79f698cc79f21c6eae8f87b305aaa91
alistairjevans <alistairjevans@gmail.com>
2021-10-30 18:08:51 +0100

    Fix branch name determination

--- setup/compute-version.ps1
+++ setup/compute-version.ps1
@@ -1,5 +1,16 @@
 # Set version suffix
-$branch = @{ $true = $env:GITHUB_REF; $false = $(git symbolic-ref --short -q HEAD) }[$NULL -ne $env:GITHUB_REF];
+$branch = "";
+
+if ($env:GITHUB_EVENT_NAME -eq "pull_request")
+{
+    # In a PR, use the 
+    $branch = "pr";
+}
+else 
+{
+    $branch = $($env:GITHUB_REF -split '/') | Select-Object -Last 1
+}
+
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:GITHUB_RUN_NUMBER, 10); $false = "local" }[$NULL -ne $env:GITHUB_RUN_NUMBER];
 $versionSuffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)).Replace('/', '-'))-$revision" }[$branch -eq "master" -and $revision -ne "local"]
 
