80f5fd2decc87271b68f10720e4d2e35734fa252
alistairjevans <alistairjevans@gmail.com>
2021-11-08 19:49:03 +0000

    Pass context as environment variable

--- build/build.ps1
+++ build/build.ps1
@@ -1,10 +1,8 @@
-[CmdletBinding()]
-param (
-    [Parameter()]
-    $ctxt
-)
-
 Import-Module $PSScriptRoot/Autofac.Build.psd1 -Force
+
+Write-Output $env:AUTOFAC_CTXT;
+
+$ctxt = ConvertFrom-Json $env:AUTOFAC_CTXT;
 
 # Build, then Pack
 Write-Output $ctxt
